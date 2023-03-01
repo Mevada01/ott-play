1286bd258bb00bf99462504dc2d1197dd11eb83e
Mevada01 <harsh.b.mevada@gmail.com>
2023-03-01 21:14:50 +0530

    updated

--- buyplan.php
+++ buyplan.php
@@ -8,12 +8,99 @@ require_once 'inc/header.php';
 
 <section class="my-lg-3 brdr">
     <div class="buyplan">
-        <div class="container-xxl ">
+        <div class="container-fluid">
             <div class="buyplan-main">
                 <div class="buyplan-wrapper">
                     <h2>Subscribe to watch all content on Gujarat Pro</h2>
-                    <div class="prem-table-wrapper">
-                        
+                    <div class="prem-table-wrappers">
+                        <div class="prem-table-wrapper">
+                            <div class="containet-bx">
+                                <!-- <div class="div"><br><br><br></div> -->
+                                <div class="contents">
+                                    <p> All Contents</p>
+                                    <span>Movies, live sports, TV, Specials</span>
+                                </div>
+                                <div class="contents">
+                                    <p> Watch on TV or Laptop</p>
+                                </div>
+                                <div class="contents">
+                                    <p> Ads free movies and shows</p>
+                                </div>
+                                <div class="contents">
+                                    <p> Number of devices that can be support</p>
+                                </div>
+                                <div class="contents">
+                                    <p>Max video quality</p>
+                                </div>
+                                <div class="contents">
+                                    <p>Max audio quality</p>
+                                </div>
+                            </div>
+                            <div class="plan-bx">
+                                <div class="superplan-bx active">
+                                    <Span class="main-plans">Super</Span>
+                                    <div class="plans">
+                                        <span> <img src="assets/img/True.png" alt="" class="small-img"></span>
+                                    </div>
+                                    <div class="plans">
+                                        <span> <img src="assets/img/True.png" alt="" class="small-img"></span>
+                                    </div>
+                                    <div class="plans">
+                                        <span> <img src="assets/img/False.png" alt="" class="small-img"></span>
+                                    </div>
+                                    <div class="plans">
+                                        <span> 2</span>
+                                    </div>
+                                    <div class="plans">
+                                        <span>Full HD (1080p)</span>
+                                    </div>
+                                    <div class="plans">
+                                        <span>Dolby 5.1</span>
+                                    </div>
+                                </div>
+                                <div class="premplan-bx">
+                                    <Span class="main-plans">Super</Span>
+                                    <div class="plans">
+                                        <span> <img src="assets/img/True.png" alt="" class="small-img"></span>
+                                    </div>
+                                    <div class="plans">
+                                        <span> <img src="assets/img/True.png" alt="" class="small-img"></span>
+                                    </div>
+                                    <div class="plans">
+                                        <span> <img src="assets/img/False.png" alt="" class="small-img"></span>
+                                    </div>
+                                    <div class="plans">
+                                        <span> 2</span>
+                                    </div>
+                                    <div class="plans">
+                                        <span>Full HD (1080p)</span>
+                                    </div>
+                                    <div class="plans">
+                                        <span>Dolby 5.1</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prem-table-price">
+                            <div class="price active">
+                                <span class="active" >Super</span>
+                                <p><b>₹899</b>/Year</p>
+                                <div class="chk-img">
+                                    <img src="assets/img/check.png" alt="">
+                                </div>
+                            </div>
+                            <div class="price">
+                                <span>Premium</span>
+                                <p><b>₹1499</b>/Year</p>
+                            </div>
+                            <div class="price">
+                                <span>Premium</span>
+                                <p><b>₹299</b>/Month</p>
+                            </div>
+                        </div>
+                        <div class="conti-btn">
+                            <button>Continue With SUPER <i class="fa-solid fa-angle-right"></i></button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -26,7 +113,7 @@ require_once 'inc/header.php';
 
 
 
-<?php   
+<?php
 // $footerData = '<script src="/js/hotel-detail.js?v"></script>';
 require_once 'inc/footer.php';
 ?>
